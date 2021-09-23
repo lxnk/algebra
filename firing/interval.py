1f90898eaccae55f3b698f89bdb70d25dd50a8c9
Alex Kashuba <o.kashuba@gmail.com>
2021-09-23 12:28:57 +0200

    Minimal arithmetics is done

--- firing/interval.py
+++ firing/interval.py
@@ -10,10 +10,23 @@ https://pyinterval.readthedocs.io/en/latest/guide.html
 https://conference.scipy.org/proceedings/scipy2008/paper_3/full_text.pdf
 """
 
+from math import inf
+
 
 class AssignmentError(ArithmeticError):
     """Base class for exceptions in this module."""
     pass
+
+
+def asinterval(c):
+    if isinstance(c, Interval):
+        return c
+    elif isinstance(c, tuple):
+        return Interval(*c)
+    elif isinstance(c, (float, int)):
+        return Interval(c, c)
+    else:
+        return AssignmentError
 
 
 class Interval:
@@ -22,7 +35,7 @@ class Interval:
     a: float = 0
     b: float = 0
 
-    def __init__(self, a: float = 0, b: float = 0):
+    def __init__(self, a: float = 0, b: float = inf):
         self.a, self.b = a, b
 
     def __repr__(self):
@@ -42,111 +55,141 @@ class Interval:
     def __eq__(self, other):
         return self.a == other.a and self.b == other.b
 
-    def __add__(self, other):
+    @staticmethod
+    def _values(other):
         if isinstance(other, Interval):
-            a, b = other.a, other.b
+            return other.a, other.b
         elif isinstance(other, (int, float)):
-            a = b = other
+            return other, other
         else:
             return ArithmeticError
+
+    def _products(self, other):
+        a, b = self._values(other)
+        products = (self.a * a, self.a * b, self.b * a, self.b * b)
+        return min(products), max(products)
+
+    def __add__(self, other):
+        a, b = self._values(other)
         return Interval(self.a + a, self.b + b)
-    #
-    # def __sub__(self, other):
-    #     other = asinterval(other)
-    #     return Interval(self.a - other.b, self.b - other.a)
-    #
-    # def __neg__(self):
-    #     return Interval(-self.b, -self.a)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        a, b = self._values(other)
+        return Interval(self.a - b, self.b - a)
+
+    def __rsub__(self, other):
+        a, b = self._values(other)
+        return Interval(a - self.b, b - self.a)
+
+    def __neg__(self):
+        return Interval(-self.b, -self.a)
 
     # def __pos__(self):
     #     return NotImplemented
     #
-    # def _products(self, other):
-    #     products = [self.a * other.a, self.a * other.b, self.b * other.a, self.b * other.b]
-    #     return min(products), max(products)
-    #
-    # def __mul__(self, other):
-    #     a, b = self._products(asinterval(other))
-    #     return Interval(a, b)
+
+    def __mul__(self, other):
+        a, b = self._products(other)
+        return Interval(a, b)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
 
-# def asfinterval(c):
-#     if issubclass(type(c), Finterval):
-#         return c
-#     else:
-#         return Finterval(c, c)
-#
-#
-# def finterval(a: float = 0, b: float = 0):
-#     if 0 <= a <= b:
-#         return Finterval(0, 0, np.float_(a), np.float_(b))
-#     elif a <= b <= 0:
-#         return Finterval(np.float_(a), np.float_(b), 0, 0)
-#     elif a <= 0 <= b:
-#         return Finterval(np.float_(a), 0, 0, np.float_(b))
-#
-#
-# class Finterval:
-#     """New numeric class that represent the doupled interval <[a..b], [c..d]>
-#     """
-#     a: np.float_ = 0
-#     b: np.float_ = 0
-#     c: np.float_ = 0
-#     d: np.float_ = 0
-#
-#     def __init__(self, a: np.float_ = 0, b: np.float_ = 0, c: np.float_ = 0, d: np.float_ = 0):
-#         self.a, self.b, self.c, self.d = a, b, c, d
-#
-#     def __repr__(self):
-#         return f"Fint({self.a}, {self.b}, {self.c}, {self.d})"
-#
-#     def __format__(self, format_spec):
-#         if format_spec == '':
-#             return f"⟨({self.a}⠤{self.b}),({self.c}⠤{self.d})⟩"
-#         elif format_spec == 'ascii':
-#             return f"<({self.a}..{self.b}),({self.c}..{self.d})>"
-#         elif format_spec == 'unicode':
-#             return f"⟨({self.a}⠤{self.b}),({self.c}⠤{self.d})⟩"
-#
-#     def __str__(self):
-#         return f"⟨[{self.a}⠤{self.b}],[{self.c}⠤{self.d}]⟩"
-#
-#     def __add__(self, other):
-#         # other = asinterval(other)
-#         return Finterval(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
-#
-#     def __sub__(self, other):
-#         other = asinterval(other)
-#         return Finterval(self.a - other.b, self.b - other.a)
-#
-#     def __isub__(self, other):
-#         other = asinterval(other)
-#         self.a -= other.b
-#         self.b -= other.a
-#         return self
-#
-#     def __rsub__(self, other):
-#         other = asinterval(other)
-#         return Interval(other.a - self.b, other.b - self.a)
-#
-#     def __neg__(self):
-#         return Interval(-self.b, -self.a)
-#
-#     # def __pos__(self):
-#     #     return NotImplemented
-#
-#     def _products(self, other):
-#         products = [self.a * other.a, self.a * other.b, self.b * other.a, self.b * other.b]
-#         return min(products), max(products)
-#
-#     def __mul__(self, other):
-#         a, b = self._products(asinterval(other))
-#         return Interval(a, b)
-#
-#     def __imul__(self, other):
-#         self.a, self.b = self._products(asinterval(other))
-#         return self
-#
-#     def __rmul__(self, other):
-#         return self.__mul__(other)
+def asfinterval(c):
+    if isinstance(c, Finterval):
+        return c
+    else:
+        if isinstance(c, Interval):
+            a, d = c.a, c.b
+        elif isinstance(c, tuple):
+            a, d = c
+        elif isinstance(c, (float, int)):
+            a = d = c
+        else:
+            return AssignmentError
 
+        if 0 <= a <= d:
+            return Finterval(0, 0, a, d)
+        elif a <= d <= 0:
+            return Finterval(a, d, 0, 0)
+        elif a <= 0 <= d:
+            return Finterval(a, 0, 0, d)
+
+
+class Finterval:
+    """New numeric class that represent the doupled interval <[a..b], [c..d]>
+    """
+    a: float = 0
+    b: float = 0
+    c: float = 0
+    d: float = 0
+
+    def __init__(self, a: float = 0, b: float = 0, c: float = 0, d: float = 0):
+        self.a, self.b, self.c, self.d = a, b, c, d
+
+    def __repr__(self):
+        return f"Fintnerval({self.a}, {self.b}, {self.c}, {self.d})"
+
+    def __format__(self, format_spec):
+        if format_spec == '':
+            return f"⟨❪{self.a}⠤{self.b}),({self.c}⠤{self.d}❫⟩"
+        elif format_spec == 'ascii':
+            return f"<({self.a}..{self.b}),({self.c}..{self.d})>"
+        elif format_spec == 'unicode':
+            return f"⟨〖{self.a}⠤{self.b}),({self.c}⠤{self.d}〗⟩"
+
+    def __str__(self):
+        return f"⟨【{self.a}⠤{self.b}】,【{self.c}⠤{self.d}】⟩"
+
+    def __eq__(self, other):
+        return self.a == other.a and self.b == other.b and self.c == other.c and self.d == other.d
+
+    @staticmethod
+    def _values(other):
+        if isinstance(other, Finterval):
+            return other.a, other.b, other.c, other.d
+        elif isinstance(other, (int, float)):
+            if other >=0:
+                return 0, 0, other, other
+            else:
+                return other, other, 0, 0
+        else:
+            return ArithmeticError
+
+    def __add__(self, other):
+        a, b, c, d = self._values(other)
+        return Finterval(self.a + a, self.b + b, self.c + c, self.d + d)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        a, b, c, d = self._values(other)
+        return Finterval(self.a - d, self.b - c, self.c - b, self.d - a)
+
+    def __rsub__(self, other):
+        a, b, c, d = self._values(other)
+        return Finterval(a - self.d, b - self.c, c - self.b, d - self.a)
+
+    def __neg__(self):
+        return Finterval(-self.d, -self.c, -self.b, -self.a)
+
+    # def __pos__(self):
+    #     return NotImplemented
+
+    def _products(self, other):
+        a, b, c, d = self._values(other)
+        products = (self.a * d + self.d * a, self.b * c + self.c * b,
+                    self.b * b + self.c * c, self.a * a + self.d * d)
+        return products
+
+    def __mul__(self, other):
+        a, b, c, d = self._products(other)
+        return Finterval(a, b, c, d)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
--- tests/test_interval.py
+++ tests/test_interval.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from .context import *
-from firing.interval import Interval
+from firing.interval import Interval, asinterval
 
 
 @pt.fixture(params=[(3, 4), (1.0, 2.0), (-3, 1)], ids=["34", "12", "-31"])
 def iv(request):
     """Create intervals out of the numbers pairs"""
-    return Interval(request.param[0], request.param[1])
-
-
-
+    return Interval(*request.param)
 
 
 # def test_roots(iv):
@@ -21,9 +18,12 @@ def iv(request):
 #
 #
 
-def test_arithmetics(iv):
-    c = 0.7
+def test_print(iv):
     i = Interval(1, 2)
     print()
-    print(iv+c)
+    print(iv, type(iv.a))
+
+
+def test_arithmetics(iv):
+    i = Interval(1, 2)
     print(iv+i)
